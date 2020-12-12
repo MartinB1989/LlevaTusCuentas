7430d23b0ab6a0642d7f2348567a3de628cdd298
MatinB <j.martin.b1989@gmail.com>
2020-12-12 17:15:04 -0300

    AppLlevaTusCuentas - 12-12-2020

---
+++ inicio.php
@@ -0,0 +1,36 @@
+<?php 
+    require "php/login.php"
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/iniciarsesion.css">
+    <link rel="stylesheet" href="css/inicio.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Peta&family=Raleway:wght@300&display=swap" rel="stylesheet">
+
+    <title>Apps Iniciar Sesión</title>
+</head>
+<body>
+    <div class="contenedor-principal">
+
+        <?php include "nav.php"?>
+
+        <h1 class="titulo">Lleva tus cuentas</h1>
+        <p class="parrafo">
+            Lleva tus cuentas es una web donde podes llevar un control de tus 
+            ingresos y gastos de forma muy sensilla, solo tenes que hacerte un usuario 
+            empezar a usarla. No vas a necesitar tutorial alguno ya que es muy intuitiva
+            y simple. <a href="registrarse.php" class="reg">Registrate</a> y empesa a usarla.
+        </p>
+    </div>
+
+
+
+
+
+    
+</body>
+</html>
--- nav.php
+++ nav.php
@@ -1,6 +1,6 @@
 
         <nav class="nav-class">
-            <a href="#" class="nav-link">Inicio</a>
+            <a href="inicio.php" class="nav-link">Inicio</a>
             <a href="iniciarsesion.php" class="nav-link">Iniciar Sesión</a>
             <a href="registrarse.php" class="nav-link">Registrarse</a>
             
